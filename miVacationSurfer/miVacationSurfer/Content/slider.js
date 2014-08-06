7688e89016f8489f111d453c694a8895b0e324b4
BigK1337 <kyleahicks@yahoo.com>
2014-08-06 16:36:04 -0400

    JQuery Update
    Got the slideshow to work.  Now to work on the fading time of each
    pictures.

---
+++ miVacationSurfer/miVacationSurfer/Content/slider.js
@@ -0,0 +1,22 @@
+﻿slideint = 1;
+slideNext = 2;
+
+$(document).ready(function() {
+    $("#slideshow > img#1").fadeIn(300);
+    startSlide();
+});
+
+function startSlide() {
+    count = $("#slideshow > img").size();
+    loop = setInterval(function () {
+        if (slideNext > count) {
+            slideNext = 1;
+            slideInt = 1;
+        }
+
+        $("#slideshow > img").fadeOut(300);
+        $("#slideshow > img#" + slideNext).fadeIn(300);
+        slideInt = slideNext;
+        slideNext = slideNext+1;
+    },3000);
+}
